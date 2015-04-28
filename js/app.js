8c4a3ed4427546382d8ec827805e48f96ad172c2
Tom Mertz & Brian Kropff <student@epicodus.com>
2015-04-28 11:48:27 -0700

    Add overlay for images and hover directive

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module("NPC", []);
+
+app.controller("NpcCtrl", function() {
+    var npc = this;
+    npc.npcs = [
+        {name: "Master Castellan", bio: "Master of the Greenest Keep. He\'s seen many battles and is a competent leader", img: "img/castellan.jpg"},
+        {name: "Governor Nighthill", bio: "Governor of the Greenest, he helps keep peace in this simple town.", img: "img/nighthill.jpg"},
+    ];
+});
+
+app.directive("shadow", function() {
+    return function(scope, element, attrs) {
+        element.bind("mouseenter", function() {
+            element.addClass("shadow");
+            element.children()[1].style.display = 'block';
+        });
+
+        element.bind("mouseleave", function() {
+            element.removeClass("shadow");
+            element.children()[1].style.display = 'none';
+
+        });
+    };
+
+});
