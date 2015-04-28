045fad7f6ffc3e2c047db94c26512108f42a4fc6
Tom Mertz & Brian Kropff <student@epicodus.com>
2015-04-28 15:48:17 -0700

    Add getting information from firebase

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 var app = angular.module("NPC", []);
 
-app.controller("NpcCtrl", function() {
+app.controller("NpcCtrl", function($timeout) {
     var npc = this;
-    npc.npcs = [
-        {name: "Master Castellan", bio: "Master of the Greenest Keep. He\'s seen many battles and is a competent leader", img: "img/castellan.jpg"},
-        {name: "Governor Nighthill", bio: "Governor of the Greenest, he helps keep peace in this simple town.", img: "img/nighthill.jpg"},
-        {name: "Leosin Erlanthar", bio: "Monk and ultimate GOOD guy in the campaign...but you never know.", img: "img/leosin.png"},
-        {name: "Langdedrosa Cyanwrath", bio: "The half-dragon that totally kicked spyro's ass.", img: "img/cyanwrath.png"},
+    // npc.npcs = [
+    //     {name: "Master Castellan", bio: "Master of the Greenest Keep. He\'s seen many battles and is a competent leader", img: "img/castellan.jpg"},
+    //     {name: "Governor Nighthill", bio: "Governor of the Greenest, he helps keep peace in this simple town.", img: "img/nighthill.jpg"},
+    //     {name: "Leosin Erlanthar", bio: "Monk and ultimate GOOD guy in the campaign...but you never know.", img: "img/leosin.png"},
+    //     {name: "Langdedrosa Cyanwrath", bio: "The half-dragon that totally kicked spyro's ass.", img: "img/cyanwrath.png"},
+    //
+    // ];
 
-    ];
+    npc.fb = new Firebase("https://crackling-fire-9889.firebaseio.com/");
+
+    npc.fb.on('value', function(data) {
+        var fbnpc = data.val() !== null ? data.val() : "No Data";
+        npc.npcs = fbnpc;
+        console.log(fbnpc);
+    });
+
+    $timeout(function() {
+
+    }, 1000);
+    //npc.fb.set(npc.npcs);
+    // fb.on("child_added", function(data) {
+    //     var name = data.val() ? data.val().name : "";
+    //     alert("My name is " + name);
+    // });
 });
 
 app.directive("shadow", function() {
